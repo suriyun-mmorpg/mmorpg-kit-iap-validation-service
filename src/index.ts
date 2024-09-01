8a4626494da88834678709a7146bb82684b8e3df
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2024-09-01 16:39:11 +0700

    feat: change validation workflow

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import fastify, { FastifyListenOptions } from 'fastify'
 import authPlugin from '@fastify/auth'
-import bearerAuthPlugin from '@fastify/bearer-auth'
 import { PrismaClient as IapValidationClient } from '../prisma/generated/iap-validation-client'
 import { IAPValidationForm } from './interfaces'
 import { IAPValidationService } from './functions'
@@ -8,19 +7,29 @@ import { IAPValidationService } from './functions'
 const secretKeys: string = process.env.SECRET_KEYS ? process.env.SECRET_KEYS : "[\"secret\"]"
 const iapValidationClient = new IapValidationClient()
 
+const validateAppAccess = async(request: any, reply: any, done: (err?: Error) => void) =>
+{
+    const header = request.headers['x-api-key']!
+    if (!header) {
+        done(new Error('No secret key'))
+        return
+    }
+    const keys = JSON.parse(secretKeys)
+    if (keys.indexOf(header) < 0) {
+        done(new Error('Invalid secret key'))
+        return
+    }
+}
+
 const functions = new IAPValidationService(iapValidationClient)
 const server = fastify({ logger: true })
     .register(authPlugin)
-    .register(bearerAuthPlugin, {
-        keys: JSON.parse(secretKeys),
-        addHook: false,
-    })
     .after(() => {
         server.get('/iap-validation-history/:userId', functions.getListApi)
 
         server.post<{ Body: IAPValidationForm }>('/internal/iap-validate', {
             preHandler: server.auth([
-                server.verifyBearerAuth!
+                validateAppAccess
             ]),
         }, functions.postIapValidationApi)
     })
